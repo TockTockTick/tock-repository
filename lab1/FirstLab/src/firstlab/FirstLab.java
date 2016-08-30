cc421a74a0b92deed6061decf7438faefc638459
Yuguan Bai <yubai@vassar.edu>
2016-08-30 15:39:55 -0400

    First snapshot

---
+++ lab1/FirstLab/src/firstlab/FirstLab.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstlab;
+
+/**
+ * A simple program for testing.
+ * @author yubai
+ */
+public class FirstLab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("First Lab");
+    }
+    
+}
